e6b51ec9f1345958d9c443a84d87c72974c6f87a
Diego Bim <hello@diegobim.dev>
2021-05-10 02:19:52 -0300

    chore: trigger bff deployment

--- packages/bff/src/routes.ts
+++ packages/bff/src/routes.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router
   .route('/')
-  .get((req, res) => res.json({ message: 'Hello World' }))
+  .get((req, res) => res.json({ message: 'Hello World from BFF' }))
   .post((req, res) => res.json({ message: 'New Post Created' }));
 
 export default router;
